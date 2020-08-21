f2ee9f6a51b3d99f9b61bfa929c3cd0e77c38491
ttyler <ttyler@mac.com>
2020-08-21 17:49:55 +0800

    Removed Account destruction
    Use encrypted password to reset the password for the account

--- RandomPasswordv3.sh
+++ RandomPasswordv3.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-		############## WARNING #################
-		# This script will erase a useraccount #
-		# based on your userName Variable      #
-		# within this script. PLEASE ENSURE    #
-		# YOU KNOW WHAT YOU ARE DOING.         #
-		#        YOU HAVE BEEN WARNED.         #
-		########################################
 
 	###############################################################
 	#	Copyright (c) 2018, D8 Services Ltd.  All rights reserved.  
@@ -30,13 +23,21 @@
 	# https://gist.github.com/earthgecko/3089509
 	# password expiry stolen from
 	# https://www.jamf.com/jamf-nation/discussions/9559
+	# branched from https://github.com/D8Services/RandomAccountPassword to randomise the
+	# password leveraging the encrypted password on the computer
 	
+	# Initially created by Tomos Tyler 17 Aug 3030
+	# Updated - Tomos Tyler 21 Aug 2020
+	#  -Altered Script to decrypt Password and re-randomise the password
+	
+# Parameter 4 = Salt Keys Required for encyption of userCredentials
+# Parameter 5 = Phrase Keys Required for encyption of userCredentials
+# Parameter 6 = IT User account to manage the password
+
 
 # Username to randomise the password Customise the following but ensure the saltKey and 
 # phraseKey are copied to the EA script
-userName="test"
-fullName="IT Support"
-home="/var/${userName}"
+userName="${6}"
 # Reset the Password when it is Over xx days old
 passWDPolicy="14"
 #EncryptedPasswordFile
@@ -45,18 +46,35 @@ prefFile="/var/db/.encryptedD8.plist"
 # Do not edit below this line
 saltKey="${4}"
 phraseKey="${5}"
-#saltKey="063f7f8eb687cde2"
-#phraseKey="7d7353d9547a8af1bf81d1be"
 
+# Example Keys
+#saltKey="063f7fccb687cde2"
+#phraseKey="7d7353cc547a8af1bf81d1be"
+
+# Check for Keys
 if [[ -z "${saltKey}" || -z "${phraseKey}" ]];then
 	echo "Keys Missing, exiting."
 	exit 1
 fi
 
-function GenerateEncryptedString() {
+# Check for the Preference File
+if [[ ! -f "${prefFile}" ]];then
+	echo "Preference File Missing."
+	exit 1
+fi
+
+## Function Encrypt String
+GenerateEncryptedString() {
 # Usage ~$ GenerateEncryptedString "String"
 echo "${1}" | openssl enc -aes256 -a -A -S "${saltKey}" -k "${phraseKey}"
 }
+
+## Function Decrypt String
+DecryptString() {
+    # Usage: ~$ DecryptString "Encrypted String" "Salt" "Passphrase"
+echo "${1}" | /usr/bin/openssl enc -aes256 -d -a -A -S "${saltKey}" -k "${phraseKey}"
+}
+
 
 if id "$userName" >/dev/null 2>&1; then
 	echo "Notice: User exists, continuing"
@@ -71,19 +89,18 @@ if id "$userName" >/dev/null 2>&1; then
 	
 	if [[ "$passwordAgeDays" -gt ${passWDPolicy} ]]; then
 		newPass=`cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9!@#*' | fold -w 32 | head -n 1`
-        	sysadminctl -deleteUser ${userName}
-		sysadminctl -addUser ${userName} -fullName "${fullName}" -UID 500 -password ${newPass} -home ${home} -admin
-		createhomedir -c 2>&1
+        	encryptedOldPass=$(defaults read "${prefFile}" pkey)
+            	oldPass=$(DecryptString "${encryptedOldPass}")
+		sysadminctl -resetPasswordFor "${userName}" -password "${newPass}" -adminUser "${userName}" -adminPassword "${oldPass}"
+		sysadminctl -newPassword ${newPass}" -oldPassword "${oldPass}"
 		dscl . -authonly ${userName} "${newPass}"
 		if [[ $? ]];then
-			echo "Successful Creation of account with new Password."
+			echo "Successful password Change of account \"${userName}\" with new Password."
 			encryptedString=$(GenerateEncryptedString "${newPass}")
 			defaults write "${prefFile}" pkey "${encryptedString}"
-			echo "The encrypted ${userName} users password is ${newPass}"
 			echo "The User ${userName} Password Age (EPOCH) is $passwordDateTime"
-			echo "Encrypted Sting: ${encryptedString}"
 		else
-			echo "FAILED Creation of account with new Password."
+			echo "FAILED to Reset account \"${userName}\" with a new Password."
 		fi
 	fi
 fi
